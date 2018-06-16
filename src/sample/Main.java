10583b09e0daaf75da64fdc97a4ad386882787f6
Phil Marcoccia <36013742+pippom@users.noreply.github.com>
2018-06-16 19:31:22 -0400

    Add CORRECT files
    Previously added wrong source files. Here are the correct ones.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("2Doer");
         primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
