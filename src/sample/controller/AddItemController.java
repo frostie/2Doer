bd2f5a0cf217aa82ff78f1af968ab89a7dff2aeb
Phil Marcoccia <36013742+pippom@users.noreply.github.com>
2018-06-21 19:09:39 -0400

    Add files via upload
    Add SignupController.java

--- src/sample/controller/AddItemController.java
+++ src/sample/controller/AddItemController.java
@@ -1,4 +1,26 @@
 package sample.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+
 public class AddItemController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView addButton;
+
+    @FXML
+    void initialize() {
+
+
+
+    }
 }
